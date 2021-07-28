654eaeadcfd62f28fee40a628478c36c300b0f05
Damilare Akinlaja <darmie@akinlaja.com>
2021-07-28 13:36:40 +0100

    ogg stream state

--- ogg/OggPage.hx
+++ ogg/OggPage.hx
@@ -35,3 +35,29 @@ extern class OggPacket {
     return untyped __cpp__("ogg_packet{}");  
   }
 }
+
+
+@:keep
+@:include('ogg/ogg.h')
+@:structAccess
+@:native("ogg_stream_state")
+extern class OggStreamState {
+  public var body_data:cpp.RawPointer<UInt8>;
+  public var body_storage:Int64;
+  public var body_fill:Int64;
+  public var body_returned:Int64;
+  public var lacing_vals:cpp.RawPointer<Int>;
+  public var granule_vals:Int64;
+  public var long_storage:Int64;
+  public var lacing_fill:Int64;
+  public var lacing_packet:Int64;
+  public var lacing_returned:Int64;
+  public var header:Array<UInt8>;
+  public var header_fill:Int;
+  public var b_o_s:Int64;
+  public var e_o_s:Int64;
+  public var serialno:Int64;
+  public var pageno:Int64;
+  public var packetno:Int64;
+  public var granulepos:Int64;
+}
